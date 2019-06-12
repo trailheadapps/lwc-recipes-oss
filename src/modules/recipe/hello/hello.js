d5ce8a21dfe374ff7b83b8d92e3d0c1bcd496d0b
muenzpraeger <mail@winkelmeyer.com>
2019-06-12 18:22:58 +0200

    Test dynamic import for module

--- src/modules/recipe/hello/hello.js
+++ src/modules/recipe/hello/hello.js
@@ -2,4 +2,18 @@ import { LightningElement } from 'lwc';
 
 export default class Hello extends LightningElement {
     greeting = 'World';
+
+    _isRendered = false;
+
+    connectedCallback() {
+        this.doSomeStuff();
+    }
+
+    async doSomeStuff() {
+        const errorPanel = await import(
+            /* webpackChunkName: "errorPanel" */ 'recipe/errorPanel'
+        );
+        // eslint-disable-next-line no-console
+        console.log(errorPanel);
+    }
 }
