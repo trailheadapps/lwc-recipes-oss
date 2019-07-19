e44da4a45e067a94a55e073ab74bcf8188f5ca57
pozil <philippe.ozil@gmail.com>
2019-07-19 13:16:00 +0200

    Added some wire tests

---
+++ src/modules/recipe/eventBubbling/__tests__/eventBubbling.test.js
@@ -0,0 +1,68 @@
+import { createElement } from 'lwc';
+import EventBubbling from 'recipe/eventBubbling';
+import { registerLdsTestWireAdapter } from '@salesforce/wire-service-jest-util';
+import getContactList from 'data/wireGetContactListProvider';
+
+const CONTACTS = [
+    {
+        Id: '0031700000pJRRSAA4',
+        Name: 'Amy Taylor',
+        Title: 'VP of Engineering',
+        Phone: '4152568563',
+        Email: 'amy@demo.net',
+        Picture: '/resources/images/demo/amy_taylor.jpg'
+    }
+];
+
+const getContactListWireAdapter = registerLdsTestWireAdapter(getContactList);
+
+describe('recipe-event-bubbling', () => {
+    afterEach(() => {
+        // The jsdom instance is shared across test cases in a single file so reset the DOM
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+        // Prevent data saved on mocks from leaking between tests
+        jest.clearAllMocks();
+    });
+
+    it('renders contact list from wire', () => {
+        // Create initial element
+        const element = createElement('recipe-event-bubbling', {
+            is: EventBubbling
+        });
+        document.body.appendChild(element);
+
+        // Send data to wire service
+        getContactListWireAdapter.emit(CONTACTS);
+
+        // Check rendered elements
+        return Promise.resolve().then(() => {
+            const contactElements = element.shadowRoot.querySelectorAll(
+                'recipe-contact-list-item-bubbling'
+            );
+            expect(contactElements.length).toBe(CONTACTS.length);
+            expect(contactElements[0].contact.Name).toBe(CONTACTS[0].Name);
+            expect(contactElements[0].contact.Title).toBe(CONTACTS[0].Title);
+        });
+    });
+
+    it('renders error panel when wire fails', () => {
+        // Create initial element
+        const element = createElement('recipe-event-bubbling', {
+            is: EventBubbling
+        });
+        document.body.appendChild(element);
+
+        // Send error to wire service
+        getContactListWireAdapter.error();
+
+        // Check rendered elements
+        return Promise.resolve().then(() => {
+            const errorPanelEl = element.shadowRoot.querySelector(
+                'recipe-error-panel'
+            );
+            expect(errorPanelEl).not.toBeNull();
+        });
+    });
+});
