ff500a94690d4b7952191cdfd081b4ed2a9d5bd4
René Winkelmeyer <muenzpraeger@users.noreply.github.com>
2019-10-07 15:30:23 +0200

    Revert "Use custom npm library for shared UI components (#39)"
    This reverts commit 31f8993dc738fed22a000cb9b81d68d8366789ee.

---
+++ src/modules/ui/output/phoneUtil.js
@@ -0,0 +1,10 @@
+// Credit goes to https://stackoverflow.com/questions/8358084/regular-expression-to-reformat-a-us-phone-number-in-javascript
+
+export function formatPhoneNumber(phoneNumberString) {
+    var cleaned = ('' + phoneNumberString).replace(/\D/g, '');
+    var match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+    if (match) {
+        return '(' + match[1] + ') ' + match[2] + '-' + match[3];
+    }
+    return null;
+}
