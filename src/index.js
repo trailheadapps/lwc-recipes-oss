ba3e11d6596e01a6df1d9e484071825453ed3e5d
René Winkelmeyer <muenzpraeger@users.noreply.github.com>
2019-10-11 16:59:00 +0200

    Edge browser support and information (#42)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import { buildCustomElementConstructor, register } from 'lwc';
+import { createElement, register } from 'lwc';
 import { registerWireService } from '@lwc/wire-service';
 import UiApp from 'ui/app';
 
 registerWireService(register);
 
-customElements.define('ui-app', buildCustomElementConstructor(UiApp));
+const app = createElement('my-app', { is: UiApp });
+// eslint-disable-next-line @lwc/lwc/no-document-query
+document.querySelector('#main').appendChild(app);
