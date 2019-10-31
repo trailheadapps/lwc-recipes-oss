67f8caa3cc99ae8cceac2000d0291a3b440413d5
Jody Bleyle <jbleyle@salesforce.com>
2019-10-31 13:51:26 -0700

    Change var name and ui-card title

--- src/modules/recipe/miscMultipleTemplates/miscMultipleTemplates.js
+++ src/modules/recipe/miscMultipleTemplates/miscMultipleTemplates.js
@@ -3,13 +3,13 @@ import templateOne from './templateOne.html';
 import templateTwo from './templateTwo.html';
 
 export default class MiscMultipleTemplates extends LightningElement {
-    templateOne = true;
+    showTemplateOne = true;
 
     render() {
-        return this.templateOne ? templateOne : templateTwo;
+        return this.showTemplateOne ? templateOne : templateTwo;
     }
 
     switchTemplate() {
-        this.templateOne = this.templateOne === true ? false : true;
+        this.showTemplateOne = !this.showTemplateOne;
     }
 }
