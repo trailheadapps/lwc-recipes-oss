b003b10a1a73a8fed418d2f5b47202711d982af6
René Winkelmeyer <muenzpraeger@users.noreply.github.com>
2020-03-05 10:28:07 +0100

    chore: upgrade lwc-services (#78)
    * chore: upgrade lwc-services
    
    * fix: remove not needed ts file
    
    * fix: add new server implementation

---
+++ scripts/server.js
@@ -0,0 +1,23 @@
+// Simple Express server setup to serve the build output
+const compression = require('compression');
+const helmet = require('helmet');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(helmet());
+app.use(compression());
+
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 3001;
+const DIST_DIR = './dist';
+
+app.use(express.static(DIST_DIR));
+
+app.use('*', (req, res) => {
+    res.sendFile(path.resolve(DIST_DIR, 'index.html'));
+});
+
+app.listen(PORT, () =>
+    console.log(`✅  Server started: http://${HOST}:${PORT}`)
+);
