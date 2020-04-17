57d155194ec3f4519e8d32d712f8f29b5b74d3d4
Alba Rivas <alba.azconarivas@gmail.com>
2020-04-17 12:24:58 +0200

    Group @api properties at the top

--- src/modules/recipe/errorPanel/errorPanel.js
+++ src/modules/recipe/errorPanel/errorPanel.js
@@ -4,11 +4,10 @@ import { reduceErrors } from 'recipe/ldsUtils';
 export default class ErrorPanel extends LightningElement {
     /** Generic / user-friendly message */
     @api friendlyMessage = 'Error retrieving data';
-
-    viewDetails = false;
-
     /** Single or array of errors */
     @api errors;
+
+    viewDetails = false;
 
     get errorMessages() {
         return reduceErrors(this.errors);
