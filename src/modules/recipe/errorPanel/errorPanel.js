2b3b05d7b0bfa703604da56004d723095f9a89f8
Alba Rivas <alba.azconarivas@gmail.com>
2020-04-17 12:27:04 +0200

    Order alphabetically

--- src/modules/recipe/errorPanel/errorPanel.js
+++ src/modules/recipe/errorPanel/errorPanel.js
@@ -2,10 +2,10 @@ import { LightningElement, api } from 'lwc';
 import { reduceErrors } from 'recipe/ldsUtils';
 
 export default class ErrorPanel extends LightningElement {
-    /** Generic / user-friendly message */
-    @api friendlyMessage = 'Error retrieving data';
     /** Single or array of errors */
     @api errors;
+    /** Generic / user-friendly message */
+    @api friendlyMessage = 'Error retrieving data';
 
     viewDetails = false;
 
