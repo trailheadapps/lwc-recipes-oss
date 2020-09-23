9d161497129a44a206d75034ca08042ee5c708bf
Alba Rivas <arivas@salesforce.com>
2020-09-23 14:53:04 +0200

    Add missing config file

---
+++ jest-sa11y-setup.js
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+const { registerSa11yMatcher } = require('@sa11y/jest');
+
+registerSa11yMatcher();
