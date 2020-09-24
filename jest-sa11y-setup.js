4bf04175a9525ee613455744355b1b7b0341bb65
Alba Rivas <arivas@salesforce.com>
2020-09-24 13:26:03 +0200

    Update setup file

--- jest-sa11y-setup.js
+++ jest-sa11y-setup.js
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) 2020, salesforce.com, inc.
- * All rights reserved.
- * SPDX-License-Identifier: BSD-3-Clause
- * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
- */
-
-const { registerSa11yMatcher } = require('@sa11y/jest');
+import { registerSa11yMatcher } from '@sa11y/jest';
 
 registerSa11yMatcher();
