c7ab6a598d24f11833fbe4883fcdf0a4b447c3f4
Philippe Ozil <5071767+pozil@users.noreply.github.com>
2020-10-08 09:57:02 +0200

    Stopped overriding setupFilesAfterEnv

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,9 @@
 // - if you don't want to use Jest i. e. from a Visual Studio Code extension
 const { jestConfig } = require('lwc-services/lib/config/jestConfig');
 
+const setupFilesAfterEnv = jestConfig.setupFilesAfterEnv || [];
+setupFilesAfterEnv.push('<rootDir>/jest-sa11y-setup.js');
+
 module.exports = {
     ...jestConfig,
     // Stubs for 3rd party components
@@ -11,5 +14,5 @@ module.exports = {
     },
     // Adding canvas mock for Chartjs tests
     setupFiles: ['jest-canvas-mock'],
-    setupFilesAfterEnv: ['<rootDir>/jest-sa11y-setup.js']
+    setupFilesAfterEnv
 };
