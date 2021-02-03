bf30fa2a3c6ccceebb3f431f0df53e391a49f50f
SidTrailhead <siddhartha.bharti321@gmail.com>
2021-02-03 10:13:45 +0530

    hgsdahgf

--- src/modules/recipe/sampleappone/sampleappone.js
+++ src/modules/recipe/sampleappone/sampleappone.js
@@ -1,11 +1,27 @@
-import { LightningElement } from 'lwc';
-
+import{ LightningElement, track } from 'lwc';
 export default class Paginator extends LightningElement {
-    handlePrevious() {
-        this.dispatchEvent(new CustomEvent('previous'));
-    }
-
-    handleNext() {
-        this.dispatchEvent(new CustomEvent('next'));
-    }
+@track responsedata;
+connectedCallback(){
+fetch('https://api.exchangeratesapi.io/latest', // End point URL
+{
+// Request type
+method:"GET",
+headers:{
+// content type
+"Content-Type": "application/json",
+// adding your access token
+"Authorization": "Bearer ",
+"Access-Control-Allow-Origin": '*'
+}
+})
+.then((response) => {
+return response.json(); // returning the response in the form of JSON
+})
+.then((jsonResponse) => {
+console.log('jsonResponse ===> '+JSON.stringify(jsonResponse));
+})
+.catch(error => {
+console.log('callout error ===> '+JSON.stringify(error));
+})
+}
 }
