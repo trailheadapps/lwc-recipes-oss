9de7b54bcb8f0f13ca2468ef2561f9674b098ec9
pozil <philippe.ozil@gmail.com>
2021-12-23 17:04:47 +0100

    fix: renamed root custom element to be constent with ui namespace

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { createElement } from 'lwc';
 import UiApp from 'ui/app';
 
-const app = createElement('my-app', { is: UiApp });
+const app = createElement('ui-app', { is: UiApp });
 // eslint-disable-next-line @lwc/lwc/no-document-query
 document.querySelector('#main').appendChild(app);
