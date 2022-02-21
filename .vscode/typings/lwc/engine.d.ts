ea87c44faadb6711ed173dd7531ec4cfd068e4c6
adityanaag3 <atopalli@salesforce.com>
2022-02-21 22:05:52 +0530

    Restore VS Code files

---
+++ .vscode/typings/lwc/engine.d.ts
@@ -0,0 +1,195 @@
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+/**
+ * Lightning Web Components core module
+ */
+declare module 'lwc' {
+
+    interface ComposableEvent extends Event {
+        composed: boolean
+    }
+
+    class HTMLElementTheGoodPart {
+        dispatchEvent(evt: ComposableEvent): boolean;
+        addEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions): void;
+        removeEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | EventListenerOptions): void;
+        getAttribute(name: string): string | null;
+        getBoundingClientRect(): ClientRect;
+        querySelector(selectors: string): HTMLElement | null
+        querySelectorAll(selectors: string): NodeListOf<HTMLElement>
+        readonly tagName: string
+        readonly classList: DOMTokenList;
+
+        // Default HTML Properties
+        dir: string;
+        id: string;
+        accessKey: string;
+        title: string;
+        lang: string;
+        hidden: boolean;
+        draggable: boolean;
+        tabIndex: number;
+
+        // Aria Properties
+        ariaAutoComplete: string | null;
+        ariaChecked: string | null;
+        ariaCurrent: string | null;
+        ariaDisabled: string | null;
+        ariaExpanded: string | null;
+        ariaHasPopUp: string | null;
+        ariaHidden: string | null;
+        ariaInvalid: string | null;
+        ariaLabel: string | null;
+        ariaLevel: string | null;
+        ariaMultiLine: string | null;
+        ariaMultiSelectable: string | null;
+        ariaOrientation: string | null;
+        ariaPressed: string | null;
+        ariaReadOnly: string | null;
+        ariaRequired: string | null;
+        ariaSelected: string | null;
+        ariaSort: string | null;
+        ariaValueMax: string | null;
+        ariaValueMin: string | null;
+        ariaValueNow: string | null;
+        ariaValueText: string | null;
+        ariaLive: string | null;
+        ariaRelevant: string | null;
+        ariaAtomic: string | null;
+        ariaBusy: string | null;
+        ariaActiveDescendant: string | null;
+        ariaControls: string | null;
+        ariaDescribedBy: string | null;
+        ariaFlowTo: string | null;
+        ariaLabelledBy: string | null;
+        ariaOwns: string | null;
+        ariaPosInSet: string | null;
+        ariaSetSize: string | null;
+        ariaColCount: string | null;
+        ariaColIndex: string | null;
+        ariaDetails: string | null;
+        ariaErrorMessage: string | null;
+        ariaKeyShortcuts: string | null;
+        ariaModal: string | null;
+        ariaPlaceholder: string | null;
+        ariaRoleDescription: string | null;
+        ariaRowCount: string | null;
+        ariaRowIndex: string | null;
+        ariaRowSpan: string | null;
+        role: string | null;
+    }
+
+    interface ShadowRootTheGoodPart extends NodeSelector {
+        mode: string;
+        readonly host: null;
+        readonly firstChild: Node | null,
+        readonly lastChild: Node | null,
+        readonly innerHTML: string,
+        readonly textContent: string,
+        readonly childNodes: Node[],
+        readonly delegatesFocus: boolean,
+        addEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions): void;
+        removeEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | EventListenerOptions): void;
+        hasChildNodes(): boolean;
+        compareDocumentPosition(otherNode: Node): number;
+        contains(otherNode: Node): boolean;
+
+        // Aria Properties
+        ariaAutoComplete: string | null;
+        ariaChecked: string | null;
+        ariaCurrent: string | null;
+        ariaDisabled: string | null;
+        ariaExpanded: string | null;
+        ariaHasPopUp: string | null;
+        ariaHidden: string | null;
+        ariaInvalid: string | null;
+        ariaLabel: string | null;
+        ariaLevel: string | null;
+        ariaMultiLine: string | null;
+        ariaMultiSelectable: string | null;
+        ariaOrientation: string | null;
+        ariaPressed: string | null;
+        ariaReadOnly: string | null;
+        ariaRequired: string | null;
+        ariaSelected: string | null;
+        ariaSort: string | null;
+        ariaValueMax: string | null;
+        ariaValueMin: string | null;
+        ariaValueNow: string | null;
+        ariaValueText: string | null;
+        ariaLive: string | null;
+        ariaRelevant: string | null;
+        ariaAtomic: string | null;
+        ariaBusy: string | null;
+        ariaActiveDescendant: string | null;
+        ariaControls: string | null;
+        ariaDescribedBy: string | null;
+        ariaFlowTo: string | null;
+        ariaLabelledBy: string | null;
+        ariaOwns: string | null;
+        ariaPosInSet: string | null;
+        ariaSetSize: string | null;
+        ariaColCount: string | null;
+        ariaColIndex: string | null;
+        ariaDetails: string | null;
+        ariaErrorMessage: string | null;
+        ariaKeyShortcuts: string | null;
+        ariaModal: string | null;
+        ariaPlaceholder: string | null;
+        ariaRoleDescription: string | null;
+        ariaRowCount: string | null;
+        ariaRowIndex: string | null;
+        ariaRowSpan: string | null;
+        role: string | null;
+    }
+
+    /**
+     * Base class for the Lightning Web Component JavaScript class
+     */
+    export class LightningElement extends HTMLElementTheGoodPart {
+        /**
+         * Called when the component is created
+         */
+        constructor();
+        /**
+         * Called when the element is inserted in a document
+         */
+        connectedCallback(): void;
+        /**
+         * Called when the element is removed from a document
+         */
+        disconnectedCallback(): void;
+        /**
+         * Called after every render of the component
+         */
+        renderedCallback(): void;
+        /**
+         * Called when a descendant component throws an error in one of its lifecycle hooks
+         */
+        errorCallback(error: any, stack: string): void;
+
+        readonly template: ShadowRootTheGoodPart;
+        readonly shadowRoot: null;
+    }
+
+    /**
+     * Decorator to mark public reactive properties
+     */
+    export const api: PropertyDecorator;
+
+    /**
+     * Decorator to mark private reactive properties
+     */
+    export const track: PropertyDecorator;
+
+    /**
+     * Decorator factory to wire a property or method to a wire adapter data source
+     * @param getType imperative accessor for the data source
+     * @param config configuration object for the accessor
+     */
+    export function wire(getType: (config?: any) => any, config?: any): PropertyDecorator;
+}
